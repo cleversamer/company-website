6180f7ccada7c690bc552f4c4edc76a60d299d53
Samer A <thedev.samer@gmail.com>
2022-11-30 18:18:06 +0200

    removed typing effect

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,17 +1,9 @@
 import styled from "styled-components";
 import VideoCarousel from "components/common/VideoCarousel";
-import Typed from "react-typed";
 
 const Hero = () => {
   return (
     <Container>
-      <Title
-        strings={["فريق توب لاين يرحب بكم!"]}
-        typeSpeed={100}
-        backSpeed={100}
-        backDelay={100}
-        loop
-      />
       <VideoCarousel />
     </Container>
   );
@@ -22,22 +14,8 @@ const Container = styled.div`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-  padding-top: 110px;
+  padding-top: 50px;
   background-color: #f9f9f9;
-`;
-
-const Title = styled(Typed)`
-  font-size: 2rem;
-  font-weight: 700;
-  padding: 0;
-  margin: 0;
-  margin-bottom: -50px;
-  color: #e8591d;
-
-  @media screen and (max-width: 768px) {
-    font-size: 1.2rem;
-    margin-bottom: -30px;
-  }
 `;
 
 export default Hero;
